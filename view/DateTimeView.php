7a5f57e9497cf704770b54314a8d04bc32b4a1d2
Fredrik Langå <fredriklanga@Fredriks-MacBook-Pro-2.local>
2019-09-17 17:18:56 +0200

    correction

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,7 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'The time is ' . date("l") . ', the ' . date("jS") . ' of ' . date("F Y");
+		$timeString = date("l") . ', the ' . date("jS") . ' of ' . date("F Y") . ', The time is ';
 
 		return '<p>' . $timeString . '</p>';
 	}
